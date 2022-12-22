d1288e2c5efc3287d44c1af79b4cccba6f702b0d
hamsty <tallesdesousacosta@gmail.com>
2022-12-22 09:09:55 -0300

    criação

---
+++ Cursor12x12.h
@@ -0,0 +1,15 @@
+#include <LedTableNxN.h>
+#include <Adafruit_NeoPixel.h>
+
+class Cursor12x12 : public LedTableNxN{
+    public:
+        Cursor12x12(int n, int dataPin, neoPixelType flags);
+        ~Cursor12x12();
+        void drawPixel(int16_t x, int16_t y, uint16_t color);
+        void setCursorPos(int16_t x, int16_t y);
+        uint32_t getPixel(int n);
+
+    private:
+        Adafruit_NeoPixel *table;
+        uint32_t *buffer;
+};
---
+++ cursor12x12.cpp
@@ -0,0 +1,83 @@
+#include <LedTableNxN.h>
+#include <Adafruit_NeoPixel.h>
+#include <Cursor12x12.h>
+
+Cursor12x12::Cursor12x12(int n, int dataPin, neoPixelType flags) : LedTableNxN(n)
+{
+    this->table = new Adafruit_NeoPixel(144, dataPin, flags);
+    this->table->begin();
+#if MESA == 1
+    this->table->show();
+#endif
+    this->buffer = (uint32_t *)malloc(n * n * sizeof(uint32_t));
+}
+
+Cursor12x12::~Cursor12x12()
+{
+    free(table);
+    free(buffer);
+}
+
+void Cursor12x12::drawPixel(int16_t x, int16_t y, uint16_t color)
+{
+    if (buffer)
+    {
+        if ((x < 0) || (y < 0) || (x >= _width) || (y >= _height))
+            return;
+
+        int16_t t;
+        switch (rotation)
+        {
+        case 1:
+            t = x;
+            x = WIDTH - 1 - y;
+            y = t;
+            break;
+        case 2:
+            x = WIDTH - 1 - x;
+            y = HEIGHT - 1 - y;
+            break;
+        case 3:
+            t = x;
+            x = y;
+            y = HEIGHT - 1 - t;
+            break;
+        }
+        if (y % 2 == 1)
+        {
+            x = WIDTH - x - 1;
+        }
+        uint8_t red = (color >> 11) << 3;
+        uint8_t green = ((color << 5) >> 10) << 2;
+        uint8_t blue = ((color << 11) >> 11) << 3;
+        uint16_t n = x + (WIDTH * y);
+        this->buffer[n] = (uint32_t)(red << 16) + (uint32_t)(green << 8) + (uint32_t)blue;
+    }
+}
+
+void Cursor12x12::setCursorPos(int16_t x, int16_t y)
+{
+    if (table)
+    {
+        x = x < 0 ? 0 : (x > WIDTH - 12 ? WIDTH - 12 : x);
+        y = y < 0 ? 0 : (y > HEIGHT - 12 ? HEIGHT - 12 : y);
+        for (int i = y; y < y + 12; y++)
+        {
+            for (int j = x; x < x + 12; x++)
+            {
+                if (i % 2 == 1)
+                {
+                    j = WIDTH - j - 1;
+                }
+                uint16_t nbuffer = i + (WIDTH * j);
+                uint16_t n12x12 = (j - x) + 12 * (i - y);
+                this->table->setPixelColor(n12x12, buffer[nbuffer]);
+            }
+        }
+    }
+}
+
+uint32_t LedTableNxN::getPixel(int n)
+{
+    return this->table->getPixelColor(n);
+}
